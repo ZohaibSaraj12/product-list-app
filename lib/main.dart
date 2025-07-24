8e6c20ed46a76c04ed3d6eba31c7b98129d627e8
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2025-07-24 20:16:21 +0500

    All files added

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_list_custom_widgets/screens/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
