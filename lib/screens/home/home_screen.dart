50b1785331c0ee02b0562b2628ab3c796fc082e1
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2025-07-25 18:16:27 +0500

    feat: add animations to the products

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -33,59 +36,73 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          // card widget to add shopping items
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 10.0,
-            margin: EdgeInsets.all(20.0),
-            borderOnForeground: true,
-            
-            child: GestureDetector(
-              onTap: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    
-                    content: Text(
-                      "You tapped ${items[index]["name"]} and its price is ${items[index]["price"]}",
+      body: AnimationLimiter(
+
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            // card widget to add shopping items
+            return AnimationConfiguration.staggeredList(
+              delay: Duration(seconds: 4),
+              position: index,
+              duration: Duration(seconds: 2),
+              child: SlideAnimation(
+                verticalOffset: 50.0,
+
+                child: ScaleAnimation(
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    backgroundColor: Colors.green,
-                    duration: Duration(seconds: 2),
-                    margin: EdgeInsets.all(15.0),
-                    behavior: SnackBarBehavior.floating,
+                    elevation: 10.0,
+                    margin: EdgeInsets.all(20.0),
+                    borderOnForeground: true,
+                    
+                    child: GestureDetector(
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            
+                            content: Text(
+                              "You tapped ${items[index]["name"]} and its price is ${items[index]["price"]}",
+                            ),
+                            backgroundColor: Colors.green,
+                            duration: Duration(seconds: 2),
+                            margin: EdgeInsets.all(15.0),
+                            behavior: SnackBarBehavior.floating,
+                          ),
+                        );
+                      },
+                      
+                      child: ListTile(
+                        
+                        leading: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Image.asset(images[index]),
+                        ),
+                        title: Text(
+                          items[index]["name"].toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        subtitle: Text(
+                          items[index]["price"].toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+                        ),
+                        trailing: IconButton(
+                          onPressed: () {
+                            // Navigation logic
+                            debugPrint("Button Clicked");
+                          },
+                          icon: Icon(Icons.arrow_forward_ios_outlined),
+                        ),
+                      ),
+                    ),
                   ),
-                );
-              },
-              
-              child: ListTile(
-                
-                leading: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Image.asset(images[index]),
-                ),
-                title: Text(
-                  items[index]["name"].toString(),
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                subtitle: Text(
-                  items[index]["price"].toString(),
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    // Navigation logic
-                    debugPrint("Button Clicked");
-                  },
-                  icon: Icon(Icons.arrow_forward_ios_outlined),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
